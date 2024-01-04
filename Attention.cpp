f0489d748c37c045eb237eb08d4d3e0bbccfc178
robert2653 <robert4812653@gmail.com>
2024-01-05 03:18:08 +0800

    change attention

--- Attention.cpp
+++ Attention.cpp
@@ -1,8 +1,8 @@
 // Construct VScode
-// 1. install vscode & msys2, check desktop path of vscode
+// 1. instaint vscode & msys2, check desktop path of vscode
 // 2. open mingw64, not ucrt64, "pacman -S --needed base-devel mingw-w64-x86_64-toolchain"
 // 3. add C:\\msys64\\mingw64\\bin to environment path
-// 4. (re)open vscode, install C/C++, run, choose g++
+// 4. (re)open vscode, instaint C/C++, run, choose g++
 // 5. open settings -> compiler -> add compilerPath "C:\\msys64\\mingw64\\bin\\g++.exe"
 
 // Make Codebook
@@ -11,43 +11,28 @@
 // Init
 #include <bits/stdc++.h>
 using namespace std;
-#define all(x) (x).begin(), (x).end()
+#define aint(x) (x).begin(), (x).end()
 #define endl "\n"
-#define lrep(i, st, n) for(int i = st; i < n; i++)
-#define rep(i, st, n) for(int i = st; i <= n; i++)
-#define sz size()
-#define pb(x) push_back(x)
-#define ppb pop_back()
-#define IO ios_base::sync_with_stdio(0); cin.tie(nullptr);
-#define init(x, k) memset(x, k, sizeof(x));
-#define vec_init(x, k) x.assign(x.size(), k);
-#define lc 2*now
-#define rc 2*now+1
-#define mid (L+R)/2
-typedef long long int ll;
+#define int long long
 typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<pii> vii;
-typedef pair<ll, ll> pll;
-typedef vector<ll> vl;
-typedef vector<pll> vll;
 typedef struct {
     int from; int to;
-    ll weight;
+    int weight;
 } edge;
 typedef struct {
-    ll sum;
+    int sum;
 } Node;
-const ll llinf = 1e18;
-const int inf = 1e9;
-const int MOD = 1e9+7;
-const int maxn = 2e5+5;
+const int llinf = 4e18;
+const int inf = 2e9;
+const int mod = 1e9 + 7;
+const int maxn = 2e5 + 5;
 
 void solve(){
 
 }
 int main(){
-    IO;
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
     int t = 1;
     cin >> t;
     while(t--){
